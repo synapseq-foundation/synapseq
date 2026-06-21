5332adf6c12f9a370116c22c1528897b996f2295
Ruan <ruan.klein@gmail.com>
2026-06-21 13:37:44 -0300

    feat: added wav/mp3 export

--- api-demo/binaural-cli/main.go
+++ api-demo/binaural-cli/main.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	synapseq "github.com/synapseq-foundation/synapseq/v4/core"
+	"github.com/synapseq-foundation/synapseq/v4/external"
 	"github.com/synapseq-foundation/synapseq/v4/spsq"
 )
 
@@ -17,23 +19,30 @@ func main() {
 	fadeIn := flag.Int("i", 0, "fade in duration")
 	fadeOut := flag.Int("o", 0, "fade out duration")
 	duration := flag.Int("d", 60, "duration in seconds")
-	file := flag.String("f", "output.wav", "output file")
+	file := flag.String("f", "", "output file")
 	flag.Parse()
 
 	fmt.Fprintln(os.Stderr, "Usage: binaural-cli -c <carrier> -b <beat> -a <amplitude> -i <fadeIn> -o <fadeOut> -d <duration> -f <file>")
 
+	// Create a synapseq context
 	ctx := synapseq.NewAppContext()
 
+	// Create a new spsq sequence
 	seq := spsq.New()
+	// Create a new preset
 	ts := seq.NewPreset("tone-set").Tone(*carrier).Binaural(*beat).Amplitude(*amplitude)
 
+	// Create a timeline builder
 	var tim *spsq.Builder
+	// Initialize timeline with fade in if greater than 0, otherwise start with the preset
 	if *fadeIn > 0 {
 		tim = seq.SilenceAt(0).PresetAt(time.Duration(*fadeIn)*time.Second, ts)
 	} else {
 		tim = seq.PresetAt(0, ts)
 	}
 
+	// Calculate the duration of the sequence
+	// Check if fade out is greater than 0 and adjust duration accordingly
 	diff := time.Duration(*duration)*time.Second - time.Duration(*fadeOut)*time.Second
 
 	if *fadeOut > 0 {
@@ -42,15 +51,47 @@ func main() {
 		tim = tim.PresetAt(diff, ts)
 	}
 
+	// Load the sequence from the timeline builder
 	loaded, err := tim.Load(ctx)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, "Error: invalid sequence\n"+err.Error())
+		os.Exit(1)
+	}
+
+	// Save the sequence to a file if specified, otherwise play it using ffplay
+	if *file != "" {
+		ext := filepath.Ext(*file)
+		if ext == ".mp3" {
+			ffmpeg, err := external.NewFFmpeg("")
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			fmt.Fprint(os.Stderr, "Converting to MP3...")
+			if err := ffmpeg.Convert(loaded, *file); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		} else {
+			if err := loaded.WAV(*file); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
+
+		fmt.Fprintln(os.Stderr, "done")
 		return
 	}
 
-	fmt.Printf("rendering %s...\n", *file)
-	if err := loaded.WAV(*file); err != nil {
-		fmt.Println(err)
+	ffplay, err := external.NewFFPlay("")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-	fmt.Println("done")
+	fmt.Fprintln(os.Stderr, "playing...")
+	if err := ffplay.Play(loaded); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Fprintln(os.Stderr, "done")
 }
